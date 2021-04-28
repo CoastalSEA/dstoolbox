ccd882bc4c5cac0d4a46225bd9d834259ccbeb13
(no author) <(no author)@ff604c47-fcad-4d14-8da5-50045e63f498>
2021-04-28 17:29:10 +0000

    Completed movement of muifunctions with some now in dstfunctions so that dstoolbox can be compiled as a toolbox without extraneous folders.
    git-svn-id: svn://192.168.0.80/dstoolbox@28 ff604c47-fcad-4d14-8da5-50045e63f498

--- Trunk/help/dstbx_func_help.m
+++ Trunk/help/dstbx_func_help.m
@@ -1,8 +1,11 @@
 %% dstoolbox function help
 % dstoolbox functions include the following:
 %%
-% *sortrows* - see <matlab:doc('dstable') dstable> documentation <br>
-% *displayDSproperties* see <matlab:doc(dsproperties') dsproperties>
-% documentaion <br>
-% *dst2tsc* see <matlab:doc('dst2tsc') dst2tsc> documentation <br>
-% *tsc2dst* see <matlab:doc('tsc2dst') tsc2dst> documentation <br>
+% * *sortrows* - see <matlab:doc('dstable') dstable> documentation
+% * *displayDSproperties* - see <matlab:doc(dsproperties') dsproperties>
+% documentaion
+% * *dst2tsc* - see <matlab:doc('dst2tsc') dst2tsc> documentation
+% * *tsc2dst* - see <matlab:doc('tsc2dst') tsc2dst> documentation
+%%
+% In addition, the _dstfunctions_ folder contains a library of functions 
+% used by the dstoolbox classes.
